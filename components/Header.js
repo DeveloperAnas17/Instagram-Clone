2038dd2daf6fddf3efd12b3c7a5a2cf5730eee76
Anas <amanabbasi3660@gmail.com>
2021-10-13 23:23:23 +0530

    add header

---
+++ components/Header.js
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+
+function Header() {
+  return (
+    <div className="shadow-sm border-b bg-white top-0 z-50 sticky">
+      <div className="flex justify-between items-center bg-white  max-w-6xl mx-5 xl:mx-auto">
+        {/* Left */}
+        <div className=" relative hidden lg:inline-grid h-14 w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Middel - Search input Field*/}
+        <div className="max-w-xs ">
+          <div className="mt-1 relative p-3 rounded-md ">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="outline-none bg-gray-50 block w-full pl-10 border-gray-300  focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className="flex items-center space-x-4 justify-end">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer " />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div className="absolute -top-1 -right-1 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+
+          <img
+            className="h-10 w-10 rounded-full cursor-pointer"
+            src="https://avatars.githubusercontent.com/u/71496353?v=4"
+            alt="Profile Pic"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
